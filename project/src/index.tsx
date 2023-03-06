f487ec5f4e0b036763f1f33b88383d31d651a046
psdhtmlcss <psdhtmlcss@mail.ru>
2023-03-06 23:20:14 +0400

    module 2 task 1

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 
+enum Settings {
+  PlacesCount = 315
+}
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
 root.render(
   <React.StrictMode>
-    <App />
+    <App placesCount = {Settings.PlacesCount} />
   </React.StrictMode>,
 );

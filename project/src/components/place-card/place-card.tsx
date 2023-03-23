f056b6d4945bb2d9852069754e12a07ef641d51c
psdhtmlcss <psdhtmlcss@mail.ru>
2023-03-23 10:39:04 +0400

    Ссылки на подробное описание предложения

--- project/src/components/place-card/place-card.tsx
+++ project/src/components/place-card/place-card.tsx
@@ -1,5 +1,7 @@
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 import { Offer } from '../../types/offer';
+import { AppRoute } from '../../const';
 
 type PlaceCardScreenProps = {
   offer: Offer;
@@ -13,7 +15,6 @@ export default function PlaceCard(props: PlaceCardScreenProps): JSX.Element {
   const {offer} = props;
   const {previewImage, price, rating, title, type, id} = offer;
   const [activeCard, setStateCard] = useState<CardStateType>({id: null});
-  console.log(activeCard);
 
   return (
     <article
@@ -21,9 +22,9 @@ export default function PlaceCard(props: PlaceCardScreenProps): JSX.Element {
       className="cities__card place-card"
     >
       <div className="cities__image-wrapper place-card__image-wrapper">
-        <a href="#">
-          <img className="place-card__image" src={previewImage} width="260" height="200" alt="Place image" />
-        </a>
+        <Link to={AppRoute.Room}>
+          <img className="place-card__image" src={previewImage} width="260" height="200" alt={title} />
+        </Link>
       </div>
       <div className="place-card__info">
         <div className="place-card__price-wrapper">
@@ -39,7 +40,7 @@ export default function PlaceCard(props: PlaceCardScreenProps): JSX.Element {
           </div>
         </div>
         <h2 className="place-card__name">
-          <a href="#">{title}</a>
+          <Link to={AppRoute.Room}>{title}</Link>
         </h2>
         <p className="place-card__type">{type}</p>
       </div>

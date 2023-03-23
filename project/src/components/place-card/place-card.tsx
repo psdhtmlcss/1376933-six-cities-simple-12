6c3b04cd0d5421bc1cb9b2ff828bb81bff624959
psdhtmlcss <psdhtmlcss@mail.ru>
2023-03-23 09:00:12 +0400

    Добавили в состояние компонента активную карточку

--- project/src/components/place-card/place-card.tsx
+++ project/src/components/place-card/place-card.tsx
@@ -1,36 +1,47 @@
+import { useState } from 'react';
 import { Offer } from '../../types/offer';
 
 type PlaceCardScreenProps = {
   offer: Offer;
 };
 
+type CardStateType = {
+  id: null | number;
+};
+
 export default function PlaceCard(props: PlaceCardScreenProps): JSX.Element {
   const {offer} = props;
+  const {previewImage, price, rating, title, type, id} = offer;
+  const [activeCard, setStateCard] = useState<CardStateType>({id: null});
+  console.log(activeCard);
 
   return (
-    <article className="cities__card place-card">
+    <article
+      onMouseEnter={() => setStateCard({id: id})}
+      className="cities__card place-card"
+    >
       <div className="cities__image-wrapper place-card__image-wrapper">
         <a href="#">
-          <img className="place-card__image" src={offer.previewImage} width="260" height="200" alt="Place image" />
+          <img className="place-card__image" src={previewImage} width="260" height="200" alt="Place image" />
         </a>
       </div>
       <div className="place-card__info">
         <div className="place-card__price-wrapper">
           <div className="place-card__price">
-            <b className="place-card__price-value">&euro;{offer.price}</b>
+            <b className="place-card__price-value">&euro;{price}</b>
             <span className="place-card__price-text">&#47;&nbsp;night</span>
           </div>
         </div>
         <div className="place-card__rating rating">
           <div className="place-card__stars rating__stars">
-            <span style={{ width: `${(offer.rating * 100) / 5}%` }}></span>
+            <span style={{ width: `${(rating * 100) / 5}%` }}></span>
             <span className="visually-hidden">Rating</span>
           </div>
         </div>
         <h2 className="place-card__name">
-          <a href="#">{offer.title}</a>
+          <a href="#">{title}</a>
         </h2>
-        <p className="place-card__type">{offer.type}</p>
+        <p className="place-card__type">{type}</p>
       </div>
     </article>
   );

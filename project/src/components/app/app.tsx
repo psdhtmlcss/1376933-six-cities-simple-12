ba867ad64067005c78758172982cc1613655ca70
T <psdhtmlcss@mail.ru>
2023-05-02 16:09:21 +0400

    useEffect

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,9 +1,10 @@
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Main from '../../pages/main/main';
 import Login from '../../pages/login/login';
 import Offer from '../../pages/offer/offer';
 import Page404 from '../../pages/page-404/page-404';
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import { AppRoute } from '../../const';
+import ScrollToTop from '../scrollToTop/scrollToTop';
 import { Offer as OfferType } from '../../types/offer';
 
 type AppScreenProps = {
@@ -11,8 +12,10 @@ type AppScreenProps = {
 }
 
 export default function App({offers}: AppScreenProps): JSX.Element {
+
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         <Route path={AppRoute.Main} element={<Main offers={offers} />} />
         <Route path={AppRoute.Login} element={<Login />} />
